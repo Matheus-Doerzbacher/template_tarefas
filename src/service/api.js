699f9a9780464b5cb279c80ced7c25015e088ef7
MathePLay <Matheus.doer@gmail.com>
2023-08-17 10:38:44 -0300

    Ajustado a nova URL da API

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://api-lista-tarefas-play.onrender.com/",
 });
 
 export default api;
