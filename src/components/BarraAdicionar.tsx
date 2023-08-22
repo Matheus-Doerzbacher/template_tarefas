3d599123af1b001a36e12cc40a702dd1921ea510
Matheus <matheus.doer@gmail.com>
2023-08-22 19:24:12 -0300

    Ajustes na BarraAdicionar

--- src/components/BarraAdicionar.tsx
+++ src/components/BarraAdicionar.tsx
@@ -1,60 +1,79 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Circle, Plus } from "lucide-react";
 import useAppData from "@/data/hooks/useAppData";
 
-interface BarraAdicionarProps{
-  addTarefa: (tarefa: string) => void,
-  compras?: boolean
+interface BarraAdicionarProps {
+    addTarefa: (tarefa: string) => void;
+    compras?: boolean;
 }
 
+export default function BarraAdicionar({
+    addTarefa,
+    compras,
+}: BarraAdicionarProps) {
+    const { tema } = useAppData();
+    const [inFocus, setInFocus] = useState("");
+    const [tarefa, setTarefa] = useState("");
 
-export default function BarraAdicionar({ addTarefa, compras }: BarraAdicionarProps) {
-  
-  const {tema} = useAppData()
-  const [inFocus, setInFocus] = useState("");
-  const [tarefa, setTarefa] = useState("");
-
-  return (
-    <div className={`w-full h-12  flex items-center rounded mb-3
-      ${tema === 'dark' ? 'bg-[#252423]' : 'bg-zinc-300'}
-    `}>
-      <label id="barra">
-        <button
-          className={`px-5 py-3 ${tema === 'dark' ? 'text-[#55a4f0]' : 'text-[#004e97]'}`}
-          onClick={() => {
-            addTarefa(tarefa), setTarefa("");
-          }}
+    return (
+        <div
+            className={`w-full h-12  flex items-center rounded mb-3
+      ${tema === "dark" ? "bg-[#252423]" : "bg-zinc-300"}
+    `}
         >
-          {!inFocus ? (
-            <Plus size={23} className="" />
-          ) : (
-            <Circle size={23} className={`${tema === 'dark' ? "hover:text-[#8ec7ff]" : "hover:text-[#0079f1]"}`} />
-          )}
-        </button>
-      </label>
+            {!inFocus ? (
+                <label className={`px-5 py-3 
+                          ${tema === "dark" ? "text-[#55a4f0]" : "text-[#004e97]" }`} 
+                        htmlFor="barra">
+                    <Plus size={23} className="" />
+                </label>
+            ) : (
+                <button
+                    className={`px-5 py-3 ${
+                        tema === "dark" ? "text-[#55a4f0]" : "text-[#004e97]"
+                    }`}
+                    onClick={() => {
+                        addTarefa(tarefa), setTarefa("");
+                    }}
+                >
+                    <Circle
+                        size={23}
+                        className={`${
+                            tema === "dark"
+                                ? "hover:text-[#8ec7ff]"
+                                : "hover:text-[#0079f1]"
+                        }`}
+                    />
+                </button>
+            )}
 
-      <input
-        id="barra"
-        type="text"
-        placeholder={compras ? "Adicinar um item" : "Adicionar uma tarefa"}
-        value={tarefa}
-        onKeyUp={(e) => {
-          if (e.code === "Enter" || e.code === "NumpadEnter") {
-            addTarefa(tarefa);
-            setTarefa("");
+            <input
+                id="barra"
+                name="barra"
+                type="text"
+                placeholder={
+                    compras ? "Adicinar um item" : "Adicionar uma tarefa"
+                }
+                value={tarefa}
+                onKeyUp={(e) => {
+                    if (e.code === "Enter" || e.code === "NumpadEnter") {
+                        addTarefa(tarefa);
+                        setTarefa("");
+                    }
+                }}
+                onChange={(i) => {
+                    setInFocus(i.target.value), setTarefa(i.target.value);
+                }}
+                className={`w-full h-full pr-2 bg-transparent outline-none 
+          ${
+              tema === "dark"
+                  ? "placeholder:text-[#55a4f0] focus:placeholder:text-zinc-100"
+                  : "placeholder:text-[#004e97] focus:placeholder:text-zinc-800"
           }
-        }}
-        onChange={(i) => {
-          setInFocus(i.target.value), setTarefa(i.target.value);
-        }}
-        className={`w-full h-full pr-2 bg-transparent outline-none 
-          ${tema === 'dark' 
-            ? "placeholder:text-[#55a4f0] focus:placeholder:text-zinc-100" 
-            : "placeholder:text-[#004e97] focus:placeholder:text-zinc-800"}
         `}
-        autoComplete="off"
-      />
-    </div>
-  );
+                autoComplete="off"
+            />
+        </div>
+    );
 }
